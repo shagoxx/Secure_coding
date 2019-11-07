48250976b39c8003d5a977f4a2c8a717e89ebe0e
root <root@vps737569.ovh.net>
2019-11-07 10:49:19 +0100

    recaptcha

--- PHP/login.php
+++ PHP/login.php
@@ -1,11 +1,16 @@
 <?php
 
+$token = $_POST['g-recaptcha-response']; //cogemos el token
 
-$_POST["username"];
-$_POST["password"];
+$secret = '6LfUfsEUAAAAALZX0saoSNykAjU1dvqlW30c8_Yh';
+$peticionJson = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$token);
+$respuestaGoogle = json_decode($peticionJson); //convierte la peticion en un json legible
 
-$password = $_POST["username"];
-$username = $_POST["password"];
+if ($respuestaGoogle->score > 0.7 ){ //comprueba el score y si es un robot o no y hace el login
+
+
+$password = $_POST["password"];
+$username = $_POST["username"];
 
 
 
@@ -15,22 +20,25 @@ $dbpass = "Admin123*";
 $db = "login";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
 
-$sql = "SELECT user,pass FROM users";
+$sql = "SELECT user,pass FROM users WHERE user = '$username'";
 
 $result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
+if($row = $result->fetch_assoc()) {
+
     if ($username == $row["user"]) {
-    echo "loginOK";
-    if (password_verify($password, $row["pass"])) {
-        echo '¡La contraseña es válida!';
-    }}
-        else {
-            echo "LOGINKO";
+     echo "Cuenta OK";
+        if (password_verify($password, $row["pass"])) {
+            echo '¡La contraseña es válida!';
+            session_start();
+            $_SESSION["inicio"]=$username;
+            header('Location: session.php');
         }
-
-
+        else {
+                    echo "contraseña INVALIDA";
+        }
+    }
 }
-
-
+else{ echo "la cuenta es invalida";}
+}
 ?>
---
+++ PHP/session.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+    if(isset($_SESSION["inicio"])){
+         echo "bienvenido".$_SESSION["inicio"];
+    }
+    else {
+        header('Location: ../index.html');
+    }
+
+?>
